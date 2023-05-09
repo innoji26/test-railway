d88a8655ece0cc8e8ac2efe94cdb6b758cb92a3c
indra purnomo <indrapurnomoa@gmail.com>
2023-05-09 23:35:41 +0700

    sc

--- app/config/db_config.js
+++ app/config/db_config.js
@@ -5,6 +5,7 @@ module.exports = {
     USER: process.env.MYSQLUSER,
     PASSWORD: process.env.MYSQLPASSWORD,
     DB: process.env.MYSQLDATABASE,
+    PORT: process.env.MYSQLPORT,
     dialect: 'mysql',
     pool: {
         max: 5,
