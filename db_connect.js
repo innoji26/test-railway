a0242b80da2152e1f9304187cf33cb4b21c5c842
indra purnomo <indrapurnomoa@gmail.com>
2023-05-09 21:22:06 +0700

    update data

--- db_connect.js
+++ db_connect.js
@@ -5,9 +5,15 @@ var con = mysql.createConnection({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log('Connected!');
+    var sql = 'CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))';
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log('Table created');
+    });
 });
