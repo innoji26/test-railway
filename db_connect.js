7caa85904fe5b9c7844402c10a086f6f1fca00aa
indra purnomo <indrapurnomoa@gmail.com>
2023-05-09 21:09:32 +0700

    test API railway

---
+++ db_connect.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+require('dotenv').config();
+
+var con = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected!');
+});
