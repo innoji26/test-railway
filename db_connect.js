1932cf64a72deda9a25b6fbd84ea8f8aab197b02
indra purnomo <indrapurnomoa@gmail.com>
2023-05-09 23:15:43 +0700

    coba

--- db_connect.js
+++ db_connect.js
@@ -1,20 +1,20 @@
-var mysql = require('mysql');
-require('dotenv').config();
+// var mysql = require('mysql');
+// require('dotenv').config();
 
-console.log('Hello welcome to siklo API');
-var con = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-});
+// console.log('Hello welcome to siklo API');
+// var con = mysql.createConnection({
+//     host: process.env.MYSQLHOST,
+//     user: process.env.MYSQLUSER,
+//     password: process.env.MYSQLPASSWORD,
+//     database: process.env.MYSQLDATABASE,
+// });
 
-con.connect(function(err) {
-    // if (err) throw err;
-    console.log('Connected!');
-    var sql = 'CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))';
-    con.query(sql, function(err, result) {
-        // if (err) throw err;
-        console.log('Table created');
-    });
-});
+// con.connect(function(err) {
+//     // if (err) throw err;
+//     console.log('Connected!');
+//     var sql = 'CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))';
+//     con.query(sql, function(err, result) {
+//         // if (err) throw err;
+//         console.log('Table created');
+//     });
+// });
